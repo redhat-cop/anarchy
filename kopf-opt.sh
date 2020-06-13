a49ca9f6b2751fd1ec348840ae9cafddafed16d0
Johnathan Kupferer <jkupfere@redhat.com>
2020-06-13 08:28:36 -0500

    Use ANARCHY_SERVICE for peering

--- kopf-opt.sh
+++ kopf-opt.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
-  
+
+# Default ANARCHY_SERVICE to HOSTNAME for running in odo
+ANARCHY_SERVICE=${ANARCHY_SERVICE:-$HOSTNAME}
+
 # Restrict watch to operator namespace.
 KOPF_NAMESPACED=true
 
 # Do not attempt to coordinate with other kopf operators.
-KOPF_STANDALONE=true
+KOPF_STANDALONE=false
+KOPF_OPTIONS="--peering=$ANARCHY_SERVICE"
+
+KOPF_NAMESPACE="$(cat /run/secrets/kubernetes.io/serviceaccount/namespace)"
+oc get kopfpeering anarchy || oc create -f - <<EOF
+apiVersion: zalando.org/v1
+kind: KopfPeering
+metadata:
+  namespace: $KOPF_NAMESPACE
+  name: $ANARCHY_SERVICE
+EOF
