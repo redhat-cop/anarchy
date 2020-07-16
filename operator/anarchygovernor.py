4ab95b672675bfa5640a13e4574c1b1aae513cee
Yordan <yvarbev@redhat.com>
2020-07-16 17:10:56 +0200

    Update anarchygovernor.py
    Added kubernetes.client.rest.ApiException and try/exept L#282

--- operator/anarchygovernor.py
+++ operator/anarchygovernor.py
@@ -274,14 +274,18 @@ class AnarchyGovernor(object):
                 run_post_datetime = datetime.strptime(run_resource['spec']['runPostTimestamp'], '%Y-%m-%dT%H:%M:%SZ')
                 if run_post_datetime + time_interval > datetime.utcnow():
                     continue
-            except:
+            except kubernetes.client.rest.ApiException as e:
                 # If run is already deleted then action may be deleted
                 if e.status != 404:
                     raise
-
-            runtime.custom_objects_api.delete_namespaced_custom_object(
-                runtime.operator_domain, runtime.api_version, runtime.operator_namespace, 'anarchyactions', action_name
-            )
+            
+            try:
+                runtime.custom_objects_api.delete_namespaced_custom_object(
+                    runtime.operator_domain, runtime.api_version, runtime.operator_namespace, 'anarchyactions', action_name
+                )
+            except kubernetes.client.rest.ApiException as e:
+                if e.status != 404:
+                    raise
 
     def cleanup_runs(self, runtime):
         time_interval = self.remove_successful_runs_after
@@ -299,7 +303,7 @@ class AnarchyGovernor(object):
                     runtime.custom_objects_api.delete_namespaced_custom_object(
                         runtime.operator_domain, runtime.api_version, runtime.operator_namespace, 'anarchyruns', run_name
                     )
-                except:
+                except kubernetes.client.rest.ApiException as e:
                     if e.status != 404:
                         raise
 
