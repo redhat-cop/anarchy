390854708f9cb67ff8af74fecb2a39da404f07ea
Johnathan Kupferer <jkupfere@redhat.com>
2021-11-03 09:46:27 -0500

    Fix typo in anarchy-runner.py

--- anarchy-runner/anarchy-runner.py
+++ anarchy-runner/anarchy-runner.py
@@ -150,7 +150,7 @@ class AnarchyRunner(object):
 
         # Extract governor and subject from AnarchyRun and set vars within
         # run spec into governor and subject for DWIM behavior.
-        anarchy_governor = anaruhy_run.pop('governor')
+        anarchy_governor = anarchy_run.pop('governor')
         anarchy_governor.update(anarchy_run['spec']['governor'])
         anarchy_subject = anarchy_run.pop('subject')
         anarchy_subject.update(anarchy_run['spec']['subject'])
